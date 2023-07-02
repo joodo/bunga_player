285c208d926f088bf5911bad68d95669152276c8
lianghanzhong <wyattliang@gmail.com>
2023-07-02 19:43:36 +0800

    refactor: use flutter mixin

--- lib/utils/value_listenable.dart
+++ lib/utils/value_listenable.dart
@@ -1,28 +1,20 @@
 import 'package:flutter/foundation.dart';
 
-class ReadonlyStreamNotifier<T> extends ValueListenable<T> {
+class ReadonlyStreamNotifier<T> extends ChangeNotifier
+    implements ValueListenable<T> {
   late final Stream<T> _stream;
-  final List<VoidCallback> _listeners = [];
   late T _value;
 
   ReadonlyStreamNotifier({required T initialValue, required Stream<T> stream}) {
     _value = initialValue;
     _stream = stream;
 
-    // TODO: rewrite like [ValueNotifier]
     _stream.listen((value) {
+      if (_value == value) return;
       _value = value;
-      for (var callback in _listeners) {
-        callback.call();
-      }
+      notifyListeners();
     });
   }
-
-  @override
-  void addListener(VoidCallback listener) => _listeners.add(listener);
-
-  @override
-  void removeListener(VoidCallback listener) => _listeners.remove(listener);
 
   @override
   T get value => _value;
@@ -64,23 +56,24 @@ class StreamNotifier<T> extends ChangeNotifier implements ValueListenable<T> {
 }
 
 class ValueNotifierWithReset<T> extends ValueNotifier<T> {
-  late T _initValue;
+  final T _initValue;
 
-  ValueNotifierWithReset(T value) : super(value) {
-    _initValue = value;
-  }
+  ValueNotifierWithReset(T value)
+      : _initValue = value,
+        super(value);
 
   void reset() {
     value = _initValue;
   }
 }
 
-class ProxyValueNotifier<T1, T2> extends ValueListenable<T1> {
+class ProxyValueNotifier<T1, T2> extends ChangeNotifier
+    implements ValueListenable<T1> {
   T1 _defaultProxyFunc(originValue) => originValue as T1;
 
   ProxyValueNotifier({
-    T1 Function(T2 originValue)? proxy,
     required ValueListenable<T2> from,
+    T1 Function(T2 originValue)? proxy,
   }) {
     final proxyFunc = proxy ?? _defaultProxyFunc;
     _value = proxyFunc(from.value);
@@ -95,21 +88,12 @@ class ProxyValueNotifier<T1, T2> extends ValueListenable<T1> {
     if (newValue == _value) return;
 
     _value = newValue;
-    for (var callback in _listeners) {
-      callback.call();
-    }
+    notifyListeners();
   }
-
-  final List<VoidCallback> _listeners = [];
-  @override
-  void addListener(VoidCallback listener) => _listeners.add(listener);
-  @override
-  void removeListener(VoidCallback listener) => _listeners.remove(listener);
 }
 
 class PrivateValueNotifier<T> extends ValueNotifier<T> {
   PrivateValueNotifier(super.value);
 
-  late final _readonly = ProxyValueNotifier<T, T>(from: this);
-  ValueListenable<T> get readonly => _readonly;
+  late final readonly = ProxyValueNotifier<T, T>(from: this);
 }
