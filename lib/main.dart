fdb76e7058dd5bbb5970719e455d6255423b46e6
lianghanzhong <wyattliang@gmail.com>
2024-02-19 00:35:38 +0800

    prov: remember window position

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,13 @@
 import 'dart:async';
+import 'dart:convert';
+import 'dart:ui';
 
 import 'package:bunga_player/services/logger.dart';
+import 'package:bunga_player/services/preferences.dart';
 import 'package:bunga_player/services/services.dart' as services;
 import 'package:bunga_player/screens/main_screen.dart';
 import 'package:bunga_player/screens/wrappers/wrap.dart';
+import 'package:bunga_player/services/services.dart';
 import 'package:bunga_player/utils/ssl_walkthrough.dart';
 import 'package:flutter/material.dart';
 import 'package:window_manager/window_manager.dart';
@@ -20,10 +24,9 @@ void main() {
 
       if (const LocalPlatform().isWindows) sslWalkthrough();
 
-      await windowManager.ensureInitialized();
-      windowManager.setMinimumSize(const Size(900, 720));
-
       await services.init();
+
+      await initWindow();
 
       final home = wrap(const MainScreen());
       final app = MaterialApp(
@@ -39,6 +42,60 @@ void main() {
     (error, stackTrace) {
       logger.e(error, stackTrace: stackTrace);
       throw error;
+    },
+  );
+}
+
+Future<void> initWindow() async {
+  await windowManager.ensureInitialized();
+
+  const minSize = Size(900, 720);
+
+  late WindowOptions windowOptions;
+  Offset? position;
+
+  try {
+    final windowInfo =
+        jsonDecode(getIt<Preferences>().get<String>('window_info')!);
+
+    windowOptions = WindowOptions(
+      size: Size(windowInfo['width'], windowInfo['height']),
+      minimumSize: minSize,
+      fullScreen: windowInfo['fullscreen'],
+    );
+    position = Offset(windowInfo['x'], windowInfo['y']);
+  } catch (e) {
+    windowOptions = const WindowOptions(
+      size: minSize,
+      minimumSize: minSize,
+      center: true,
+      fullScreen: false,
+    );
+  }
+
+  await windowManager.waitUntilReadyToShow(windowOptions, () async {
+    if (position != null) await windowManager.setPosition(position);
+    await windowManager.show();
+    await windowManager.focus();
+  });
+
+  AppLifecycleListener(
+    onExitRequested: () async {
+      final position = await windowManager.getPosition();
+      final size = await windowManager.getSize();
+      final fullscreen = await windowManager.isFullScreen();
+
+      await getIt<Preferences>().set(
+        'window_info',
+        jsonEncode({
+          'x': position.dx,
+          'y': position.dy,
+          'width': size.width,
+          'height': size.height,
+          'fullscreen': fullscreen,
+        }),
+      );
+      return AppExitResponse.exit;
     },
   );
 }
