b89c8ae1d7c095aec21b0e003cfc88147a75084b
lianghanzhong <wyattliang@gmail.com>
2024-03-13 14:36:27 +0800

    fix: use new api too early

--- lib/screens/dialogs/others_dialog.dart
+++ lib/screens/dialogs/others_dialog.dart
@@ -172,7 +172,7 @@ class OthersDialogState extends State<OthersDialog> {
               margin: const EdgeInsets.symmetric(horizontal: 8),
               clipBehavior: Clip.hardEdge,
               color: ElevationOverlay.applySurfaceTint(
-                themeData.colorScheme.surfaceContainerHighest,
+                themeData.colorScheme.surfaceVariant,
                 themeData.colorScheme.surfaceTint,
                 0,
               ),
