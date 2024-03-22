6721cfcc21b89e1daadc078bdcf95666c271bc05
lianghanzhong <wyattliang@gmail.com>
2024-03-22 15:24:44 +0800

    add macos deploy

--- bin/deploy.dart
+++ bin/deploy.dart
@@ -2,6 +2,9 @@
 
 import 'dart:io';
 
+// ignore: depend_on_referenced_packages
+import 'package:yaml/yaml.dart';
+
 void main() async {
   if (Platform.isMacOS) {
     print('Generating dmg file...');
@@ -19,12 +22,23 @@ void main() async {
       if (result.exitCode != 0) {
         print('Error: ${result.stderr}');
       } else {
-        Process.run(
-            'open', ['build/macos/Build/Products/Release/bunga_player.app']);
+        final version = await getVersion();
+
+        Process.run('mv', [
+          'build/macos/Build/Products/Release/Bunga Player $version.dmg',
+          'build/macos/Build/Products/Release/bunga_player_installer_${version.replaceAll('.', '_')}_macos.dmg',
+        ]);
+        Process.run('open', ['build/macos/Build/Products/Release/']);
       }
     } on ProcessException {
       print(
-          'Error: please install "creaate-dmg" first:\n\nnpm install --global create-dmg\n');
+          'Error: please install "create-dmg" first:\n\nnpm install --global create-dmg\n');
     }
   }
+}
+
+Future<String> getVersion() async {
+  File f = File('pubspec.yaml');
+  Map yaml = loadYaml(await f.readAsString());
+  return yaml['version'];
 }
