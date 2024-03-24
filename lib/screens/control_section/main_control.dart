c4079977bbf21bb78a52335b477f112c1c84fe2a
lianghanzhong <wyattliang@gmail.com>
2024-03-24 17:15:41 +0800

    update comment

--- lib/screens/control_section/main_control.dart
+++ lib/screens/control_section/main_control.dart
@@ -135,9 +135,9 @@ class _MainControlState extends State<MainControl> {
         ),
         const SizedBox(width: 8),
 
-        // FIXME: wait bug fixed then use PopupMenuButton
+        // HACK: wait bug fixed then use PopupMenuButton
         // see https://github.com/flutter/flutter/issues/144669
-        // TODO: change animation to none
+        // then change animation to none
         mock.MyPopupMenuButton(
           icon: const Icon(Icons.more_horiz),
           tooltip: '',
