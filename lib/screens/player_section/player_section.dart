0a4f92f3a8fd73af61f559e03c7242d61d97179d
lianghanzhong <wyattliang@gmail.com>
2024-06-27 12:04:34 +0800

    fix: Overflow issue when adjusting video progress by sliding

--- lib/screens/player_section/player_section.dart
+++ lib/screens/player_section/player_section.dart
@@ -1,7 +1,6 @@
 import 'dart:async';
 
-import 'package:bunga_player/player/actions.dart';
-import 'package:bunga_player/ui/actions.dart';
+import 'package:bunga_player/screens/player_section/play_gesture_detector.dart';
 import 'package:bunga_player/ui/providers.dart';
 import 'package:bunga_player/player/providers.dart';
 import 'package:bunga_player/screens/player_section/danmaku_player.dart';
@@ -9,7 +8,6 @@ import 'package:bunga_player/screens/player_section/placeholder.dart';
 import 'package:bunga_player/screens/player_section/popmoji_player.dart';
 import 'package:bunga_player/player/service/service.dart';
 import 'package:bunga_player/services/services.dart';
-import 'package:bunga_player/utils/business/platform.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:screen_brightness/screen_brightness.dart';
@@ -38,152 +36,13 @@ class _PlayerSectionState extends State<PlayerSection> {
           curve: Curves.easeOutCubic,
           child: const PlayerPlaceholder(),
         ),
-        const _GestureDetector(),
+        const PlayGestureDetector(),
         const Positioned(
           top: 48,
           left: 24,
           child: _VolumePopup(),
         ),
       ],
-    );
-  }
-}
-
-class _GestureDetector extends StatefulWidget {
-  const _GestureDetector();
-
-  @override
-  State<_GestureDetector> createState() => _GestureDetectorState();
-}
-
-class _GestureDetectorState extends State<_GestureDetector> {
-  Offset? _dragStartPoint;
-
-  Duration? _dargStartVideoPosition;
-  double? _dragStartDeviceValue;
-  double? _dragUpdatedDeviceValue;
-
-  @override
-  void initState() {
-    ScreenBrightness().setAnimate(false);
-    super.initState();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    if (kIsDesktop) {
-      return GestureDetector(
-        onDoubleTap: Actions.handler(
-            context, SetFullScreenIntent(!context.read<IsFullScreen>().value)),
-        onTap: () => Actions.maybeInvoke(context, const TogglePlayIntent()),
-      );
-    }
-
-    final screenWidth = MediaQuery.sizeOf(context).width;
-    int getTargetPosition(Offset localPosition) {
-      final videoDuration = context.read<PlayDuration>();
-      final dx = localPosition.dx - _dragStartPoint!.dx;
-      final position = _dargStartVideoPosition!.inMilliseconds +
-          videoDuration.value.inMilliseconds * dx / screenWidth;
-      return position.toInt();
-    }
-
-    final showHud = context.read<ShouldShowHUD>();
-    final gestureDetector = GestureDetector(
-      onDoubleTap: () {
-        if (context.read<PlayStatus>().isPlaying) {
-          context.read<ShouldShowHUD>().mark();
-        }
-        Actions.maybeInvoke(context, const TogglePlayIntent());
-      },
-      onTap: () {
-        if (showHud.value) {
-          showHud.reset();
-        } else {
-          showHud.mark();
-        }
-      },
-      onHorizontalDragStart: (details) {
-        context.read<ShouldShowHUD>().lockUp('drag');
-        _dragStartPoint = details.localPosition;
-
-        _dargStartVideoPosition = context.read<PlayPosition>().value;
-        Actions.invoke(
-          context,
-          StartDraggingProgressIntent(_dargStartVideoPosition!),
-        );
-      },
-      onHorizontalDragUpdate: (details) {
-        final position = getTargetPosition(details.localPosition);
-        Actions.invoke(
-          context,
-          DraggingProgressIntent(Duration(milliseconds: position.toInt())),
-        );
-      },
-      onHorizontalDragEnd: (details) {
-        final position = getTargetPosition(details.localPosition);
-        Actions.invoke(
-          context,
-          FinishDraggingProgressIntent(
-            Duration(milliseconds: position.toInt()),
-          ),
-        );
-
-        _dargStartVideoPosition = null;
-        _dragStartPoint = null;
-        context.read<ShouldShowHUD>().unlock('drag');
-      },
-      onVerticalDragStart: (details) async {
-        _dragStartPoint = details.localPosition;
-
-        if (_dragStartPoint!.dx < screenWidth / 2) {
-          // Adjust brightness
-          _dragStartDeviceValue = await ScreenBrightness().current;
-        } else {
-          // Adjust volume
-          _dragStartDeviceValue = await VolumeController().getVolume();
-        }
-      },
-      onVerticalDragUpdate: (details) {
-        if (_dragStartDeviceValue == null) return;
-
-        final delta = (_dragStartPoint!.dy - details.localPosition.dy) / 200;
-        final target = _dragStartDeviceValue! + delta;
-
-        if (_dragUpdatedDeviceValue != null &&
-            (target - _dragUpdatedDeviceValue!).abs() < 0.05) return;
-        _dragUpdatedDeviceValue = target.clamp(0.0, 1.0);
-
-        if (_dragStartPoint!.dx < screenWidth / 2) {
-          // Adjust brightness
-          context
-              .read<JustAdjustedByShortHand>()
-              .markWithAction(ShortHandAction.deviceBrightness);
-          ScreenBrightness().setScreenBrightness(_dragUpdatedDeviceValue!);
-        } else {
-          // Adjust volume
-          context
-              .read<JustAdjustedByShortHand>()
-              .markWithAction(ShortHandAction.deviceVolume);
-          VolumeController().setVolume(
-            _dragUpdatedDeviceValue!,
-            showSystemUI: false,
-          );
-        }
-      },
-      onVerticalDragEnd: (details) {
-        _dragStartPoint = null;
-        _dragStartDeviceValue = null;
-      },
-    );
-
-    return Selector<PlayVideoEntry, bool>(
-      selector: (context, videoEntryNotifer) => videoEntryNotifer.value == null,
-      builder: (context, ignoring, child) => IgnorePointer(
-        ignoring: ignoring,
-        child: child,
-      ),
-      child: gestureDetector,
     );
   }
 }
