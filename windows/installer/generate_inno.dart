918ea0869653dd5d3499dadc708404bc4eed9f3c
lianghanzhong <wyattliang@gmail.com>
2024-07-06 19:46:17 +0800

    fix: inno chinese translation missing

--- windows/installer/generate_inno.dart
+++ windows/installer/generate_inno.dart
@@ -36,7 +36,7 @@ void main() async {
       InnoSetupLanguages().english,
       InnoSetupLanguage.custom(
         'chinesesimplified',
-        File('compiler:Languages/ChineseSimplified.isl'),
+        File('windows/installer/ChineseSimplified.isl'),
       ),
     ],
     icon: InnoSetupIcon(
