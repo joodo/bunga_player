e62dd34828f3f5c7f270fbc8abf9f41ea776d22b
Joodo <wyattliang@gmail.com>
2025-03-10 18:50:27 +0800

    fix: github action windows mirror issue walkthrough

--- windows/installer/remove_unnecessary_files.dart
+++ windows/installer/remove_unnecessary_files.dart
@@ -6,23 +6,40 @@ void main() async {
   const path = 'build/windows/x64/runner/Release/';
   const files = [
     'libagora_audio_beauty_extension.dll',
+    'libagora_audio_beauty_extension.dll.lib',
     'libagora_clear_vision_extension.dll',
+    'libagora_clear_vision_extension.dll.lib',
     'libagora_content_inspect_extension.dll',
+    'libagora_content_inspect_extension.dll.lib',
     'libagora_pvc_extension.dll',
-    'libagora_spatial_audio_extension.dll',
-    'libagora_segmentation_extension.dll',
     'libagora_face_capture_extension.dll',
+    'libagora_face_capture_extension.dll.lib',
     'libagora_face_detection_extension.dll',
+    'libagora_face_detection_extension.dll.lib',
+    'libagora_lip_sync_extension.dll',
+    'libagora_lip_sync_extension.dll.lib',
     'libagora_screen_capture_extension.dll',
+    'libagora_screen_capture_extension.dll.lib',
+    'libagora_segmentation_extension.dll',
+    'libagora_segmentation_extension.dll.lib',
+    'libagora_spatial_audio_extension.dll',
+    'libagora_spatial_audio_extension.dll.lib',
     'libagora_video_quality_analyzer_extension.dll',
+    'libagora_video_quality_analyzer_extension.dll.lib',
     'libagora_video_encoder_extension.dll',
+    'libagora_video_encoder_extension.dll.lib',
     'libagora_video_decoder_extension.dll',
+    'libagora_video_decoder_extension.dll.lib',
     'libagora_video_av1_decoder_extension.dll',
+    'libagora_video_av1_decoder_extension.dll.lib',
+    'libagora_video_av1_encoder_extension.dll',
+    'libagora_video_av1_encoder_extension.dll.lib',
     'video_enc.dll',
     'video_dec.dll',
+    'libagora-wgc.dll',
   ];
 
-  for (var filename in files) {
+  for (final filename in files) {
     File file = File('$path$filename');
     if (await file.exists()) {
       await file.delete();
@@ -31,4 +48,14 @@ void main() async {
       print('File does not exist: $filename');
     }
   }
+
+  // Github action walkthrough
+  // See https://github.com/actions/runner-images/issues/10004
+  Directory('windows/gb_action_walkthrough').listSync().forEach((element) {
+    if (element is File) {
+      final newFilePath = '$path${element.uri.pathSegments.last}';
+      element.copySync(newFilePath);
+      print('File copyed: ${element.path} -> $newFilePath');
+    }
+  });
 }
