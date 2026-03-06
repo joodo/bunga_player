e372f385fd096eb5a08b6f912f8a493e1a90b182
lianghanzhong <wyattliang@gmail.com>
2026-03-06 18:25:55 +0800

    trivial: remove inaccurate information

--- lib/screens/dialogs/settings/advanced.dart
+++ lib/screens/dialogs/settings/advanced.dart
@@ -122,7 +122,7 @@ class _PlayerBackedRadiosState extends State<_PlayerBackedRadios> {
             ),
             RadioListTile<PlayerBackend>(
               title: Text('Agora Media Player（不稳定）'),
-              subtitle: Text('在语音通话时，有效减少外放带来的回音。同时，耗费系统资源少，对老旧设备更友好。'),
+              subtitle: Text('在语音通话时，有效减少外放带来的回音。'),
               value: .agoraMediaPlayer,
               enabled: !_isBusy,
             ),
