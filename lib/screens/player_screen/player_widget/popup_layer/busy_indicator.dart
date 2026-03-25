d99763fa46af6252f6768014b0c4a5882e02cb71
lianghanzhong <wyattliang@gmail.com>
2026-03-25 22:15:32 +0800

    prov: busy indicator UI

--- lib/screens/player_screen/player_widget/popup_layer/busy_indicator.dart
+++ lib/screens/player_screen/player_widget/popup_layer/busy_indicator.dart
@@ -16,52 +16,93 @@ class BusyIndicator extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Consumer3<
+    return Consumer4<
       ShouldShowHUDNotifier,
       BusyStateNotifier,
-      PendingWatcherIds?
+      PendingWatcherIds?,
+      IsSyncPlaying?
     >(
-      builder: (context, showHudNotifier, isBusyNotifier, pendingIds, child) {
-        String? getText() {
-          if (showHudNotifier.value) return null;
+      builder:
+          (
+            context,
+            showHudNotifier,
+            isBusyNotifier,
+            pendingIds,
+            isSyncPlaying,
+            child,
+          ) {
+            String getText() {
+              if (isBusyNotifier.isBusy) return '正在加载';
 
-          if (isBusyNotifier.isBusy) return '正在加载';
+              if (pendingIds == null || pendingIds.isEmpty) return '';
 
-          if (pendingIds == null || pendingIds.isEmpty) return null;
+              if (pendingIds.length > 1) return '正在等待多人缓冲';
 
-          if (pendingIds.length > 1) return '正在等待多人缓冲';
+              final user = context.read<Watchers>().firstWhereOrNull(
+                (element) => element.id == pendingIds.first,
+              );
+              if (user?.isCurrent(context) == true) {
+                return '正在缓冲';
+              }
+              return '正在等待 ${user?.name ?? '神秘人'} 缓冲';
+            }
 
-          final user = context.read<Watchers>().firstWhereOrNull(
-            (element) => element.id == pendingIds.first,
-          );
-          if (user?.isCurrent(context) == true) {
-            return '正在缓冲';
-          }
-          return '正在等待 ${user?.name ?? '神秘人'} 缓冲';
-        }
+            final text = getText();
 
-        final text = getText();
-        return PopupWidget(
-          showing: text != null,
-          layoutBuilder: (context, child) =>
-              child.padding(all: 24.0).alignment(.bottomLeft),
-          popupBuilder: (context) =>
-              [
-                    CircularProgressIndicator(
-                      strokeCap: StrokeCap.round,
-                      strokeWidth: 2.0,
-                      color: Theme.of(context).colorScheme.onPrimary,
-                    ).constrained(height: 12.0, width: 12.0),
-                    Text(text ?? ''),
-                  ]
-                  .toRow(
-                    separator: const SizedBox(width: 12.0),
-                    mainAxisSize: .min,
-                  )
-                  .padding(vertical: 8.0, left: 20.0, right: 24.0)
-                  .blurToast(),
-        );
-      },
+            final shouldPop =
+                text.isNotEmpty &&
+                !showHudNotifier.value &&
+                isSyncPlaying?.value != true;
+            final shouldCenter = text.isNotEmpty && !shouldPop;
+            return PopupWidget(
+              showing: shouldPop,
+              layoutBuilder: (context, popup) =>
+                  popup.padding(all: 24.0).alignment(.bottomLeft),
+              popupBuilder: (context) =>
+                  [
+                        CircularProgressIndicator(
+                          strokeCap: StrokeCap.round,
+                          strokeWidth: 2.0,
+                          color: Theme.of(context).colorScheme.onPrimary,
+                        ).constrained(height: 12.0, width: 12.0),
+                        Text(text),
+                      ]
+                      .toRow(
+                        separator: const SizedBox(width: 12.0),
+                        mainAxisSize: .min,
+                      )
+                      .padding(vertical: 8.0, left: 20.0, right: 24.0)
+                      .blurToast(),
+              child:
+                  [
+                        CircularProgressIndicator(
+                          strokeCap: StrokeCap.round,
+                        ).constrained(height: 20.0, width: 20.0),
+                        Text(
+                          text,
+                          style: Theme.of(context).textTheme.titleLarge,
+                        ),
+                      ]
+                      .toRow(
+                        separator: const SizedBox(width: 16.0),
+                        mainAxisSize: .min,
+                      )
+                      .padding(horizontal: 24.0, vertical: 12.0)
+                      .boxShadow(
+                        color: Theme.of(context).colorScheme.shadow,
+                        spreadRadius: 12.0,
+                        blurRadius: 36.0,
+                      )
+                      .opacity(shouldCenter ? 1.0 : 0.0, animate: true)
+                      .animate(
+                        shouldCenter
+                            ? const Duration(milliseconds: 1500)
+                            : Duration.zero,
+                        Curves.easeOutCubic,
+                      )
+                      .center(),
+            );
+          },
     );
   }
 }
